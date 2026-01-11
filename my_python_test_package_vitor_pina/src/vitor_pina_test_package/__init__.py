1261edd74ebc544ede9217f3083fff710b644971
vitorpina <vitorspina1@gmail.com>
2026-01-11 18:26:14 +0000

    removing tests

--- my_python_test_package_vitor_pina/src/vitor_pina_test_package/__init__.py
+++
@@ -1,5 +0,0 @@
-from .utils_lib import print_hello_from_package
-
-__all__ = ["print_hello_from_package"]
-
-__version__ = "1.0.0"
--- my_python_test_package_vitor_pina/src/vitor_pina_test_package/utils_lib.py
+++
@@ -1,2 +0,0 @@
-def print_hello_from_package():
-    print("hello from package Vitor Pina 8 jan 2026 21:41")
